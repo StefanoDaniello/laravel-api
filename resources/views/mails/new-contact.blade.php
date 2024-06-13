bbb32dad0f8bec9cfde02559ca105585d961a7b7
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-06-13 11:04:41 +0200

    add mails

---
+++ resources/views/mails/new-contact.blade.php
@@ -0,0 +1,7 @@
+<h1>Ciao Amministratore!</h1>
+<p>
+    Hai ricevutto un nuovo messaggio dal tuo sito <br>
+    <strong>Nome:</strong><span>{{$leadr->name}}</span><br>
+    <strong>Email:</strong><span>{{$leadr->email}}</span><br>
+    <strong>Messaggio:</strong><span>{{$leadr->message}}</span>
+</p>
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\LeadController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,6 +19,9 @@ use App\Http\Controllers\Api\CategoryController;
 Route::get('posts',[PostController::class,'index']);
 Route::get('posts/{slug}',[PostController::class,'show']);
 Route::get('categories', [CategoryController::class, 'index']);
+Route::post('contacts',[LeadController::class,'store']);
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
